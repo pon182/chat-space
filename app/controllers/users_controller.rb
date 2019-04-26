1aec4eeb77a8b956f78aa34e02afea4f472f7923
pon182 <pxnxt_182@hotmail.com>
2019-04-26 13:44:39 +0900

    Usercontroller index new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
-    
+    def index
+      respond_to do |format|
+        format.html 
+        format.json
+      end      
+    end
+
+  
+
+
     def edit
     end
 
