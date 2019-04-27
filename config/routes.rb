e585d71d9354b0254682a5450c3ab82b3268eb34
pon182 <pxnxt_182@hotmail.com>
2019-04-27 15:36:17 +0900

    Routes Initialize

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'groups#index'
   
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index,:edit, :update]
   resources :groups,only: [:index, :new, :create, :edit, :update] do
      resources :messages, only: [:index, :create]
-     resources :users, only:[:index]
   end
 end
