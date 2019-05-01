03bb6b47f6142e53b66563f3393744fefa4dbea8
pon182 <pxnxt_182@hotmail.com>
2019-05-01 11:37:03 +0900

    new line

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,68 +22,61 @@ $(function (){
   }
 
   $('#new_message').on('submit', function(e){
-    e.preventDefault();
-    $('.body-right--main').animate({scrollTop: $('.body-right--main')[0].scrollHeight}, 'fast');
-   
-    var formData = new FormData(this);
-    var href = window.location.href;
-    
+        e.preventDefault();
+        $('.body-right--main').animate({scrollTop: $('.body-right--main')[0].scrollHeight}, 'fast');
+      
+        var formData = new FormData(this);
+        var href = window.location.href;
+        
 
-    $.ajax({
-      url: href,
-      type: 'POST',
-      data: formData,
-      dataType:'json',
-      processData: false,
-      contentType: false,
-    })
+        $.ajax({
+          url: href,
+          type: 'POST',
+          data: formData,
+          dataType:'json',
+          processData: false,
+          contentType: false,
+        })
 
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.new_message')[0].reset();
-      $('.upper-info').append(html);
-      $('.form__submit').removeAttr('disabled');
-    })
+        .done(function(data){
+          var html = buildHTML(data);
+          $('.new_message')[0].reset();
+          $('.upper-info').append(html);
+          $('.form__submit').removeAttr('disabled');
+        })
 
-    .fail(function(){
-      $('.form__submit').attr('disabled', false);
-      alert('テキスト入力')
-    })
+        .fail(function(){
+          $('.form__submit').attr('disabled', false);
+          alert('テキスト入力')
+        })
   });
-  
 
   $(function(){
+      var reloadMessages = function(messages) {
+        last_message_id =$('p:last').data("message-comment")
+        var groupId = $('p').data("group-id")
+
+        var url   = location.pathname;
+        var groupId = $('p').data("group-id")
+
+        if(url == `/groups/${groupId}/messages`){
+        // var group_id = location.pathname.split('/')[2];
+        // var url = 'api/messages'
+        $.ajax({
+          url: `/groups/${groupId}/api/messages`,
+          type: 'get',
+          dataType: 'json',
+          data: {id: last_message_id}
+        })
     
-  
-
-    var reloadMessages = function(messages) {
-
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    last_message_id =$('p:last').data("message-comment")
-    var groupId = $('p').data("group-id")
-
-           var url   = location.pathname;
-    var groupId = $('p').data("group-id")
-
-    if(url == `/groups/${groupId}/messages`){
-    // var group_id = location.pathname.split('/')[2];
-    // var url = 'api/messages'
-    $.ajax({
-      url: `/groups/${groupId}/api/messages`,
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    
-  
     .done(function(messages) {
-      messages.forEach(function(message){
-        var insertHTML = buildHTML(message);
-      $(".upper-info").append(insertHTML);
-      var message =$('p:last').data("message-comment")
-      if (message > last_message_id){
-      $('.body-right--main').animate({scrollTop: $('.body-right--main')[0].scrollHeight}, 'fast')}
-      })
+        messages.forEach(function(message){
+          var insertHTML = buildHTML(message);
+        $(".upper-info").append(insertHTML);
+        var message =$('p:last').data("message-comment")
+        if (message > last_message_id){
+        $('.body-right--main').animate({scrollTop: $('.body-right--main')[0].scrollHeight}, 'fast')}
+        })
     }) 
 
     .fail(function() {
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,6 @@ class MessagesController < ApplicationController
     before_action :set_group
 
     def index
-
         @message = Message.new
         @messages = @group.messages.includes(:user)
     end
